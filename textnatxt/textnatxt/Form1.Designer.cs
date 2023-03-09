5743e5b38a9e6976ac7185dbbe926d9fa70540a6
MartinSyrovy <97514843+syrovym05@users.noreply.github.com>
2023-03-09 13:11:08 +0100

    Add files via upload

---
+++ textnatxt/textnatxt/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace textnatxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Vyber";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(45, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(258, 277);
+            this.listBox1.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(376, 67);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(273, 277);
+            this.listBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Všechny záznamy";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(430, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Záznamy s mzndou pod 17300";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ textnatxt/textnatxt/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textnatxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Path.GetDirectoryName(Application.ExecutablePath);
+
+            
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(ofd.FileName, Encoding.GetEncoding("windows-1250"));
+                
+                StreamWriter sw = new StreamWriter("best.txt", false, Encoding.GetEncoding("windows-1250"));
+
+                string s;
+                string best = "0";
+                int maxmzda = 0;
+                int pocetzen = 0;
+                int soucet = 0;
+                int pocetlidi = 0;
+                string[] slova;
+                listBox1.Items.Clear();
+                listBox2.Items.Clear();
+                while (!sr.EndOfStream)
+                {
+                    s = sr.ReadLine();  
+                    while(s.Contains("\""))             //odstraneni uvozovek
+                    {
+                        s = s.Replace("\"","");
+                    }                   
+                    listBox1.Items.Add(s);
+
+                    slova = s.Split(',');                    
+                    s = Zaznam(slova);                
+
+                    int mzda = Convert.ToInt32(slova[4]);
+                    int vek = Convert.ToInt32(slova[3]);
+                    soucet += vek;
+                    pocetlidi++;
+
+                    if (mzda > maxmzda)
+                    {
+                        maxmzda = mzda;                        
+                        best = s;
+                    }
+
+                    if(mzda < 17300)
+                    {
+                        listBox2.Items.Add(s);
+                    }
+
+                    if(slova[2]=="Female") pocetzen++;
+                    
+                }
+                
+                sw.WriteLine(best);                
+                sw.WriteLine("Prumerný věk: "+ soucet/pocetlidi);
+                sw.Close();
+
+                MessageBox.Show("Pocet zen: " + pocetzen, "Informace", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        int CZK(double i)
+        {
+            return Convert.ToInt32(Math.Round(i*22.35));            
+        }
+
+        string Zaznam(string[] pole)
+        {            
+            pole[4] = CZK(Convert.ToDouble(pole[4])).ToString();
+            string s = "";
+            for (int i = 0; i < pole.Length; i++)
+            {               
+                s += pole[i];
+                if (i < pole.Length - 1) s += ",";
+            }
+
+            return s;
+        }
+    }
+}
